987a1fb039b67f68d10c53cdb4c1fd95df5331de
zae257 <36521467+zae257@users.noreply.github.com>
2018-04-05 17:51:49 -0400

    update file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var playerList = [];
+var connectCounter = 0;
+var readyCounter = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/lobby.html');
@@ -9,11 +11,20 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('player name', function(name){
-    io.emit('chat message', name);
-	console.log(name);
+	 playerList.push(name);
+    io.emit('chat message', playerList);
+	
   });
+  socket.on('player ready', function(){
+	readyCounter++;	
+	console.log("Read Counter is: " + readyCounter );
 });
-    
+
+
+socket.on('disconnect', function() { connectCounter--; console.log(connectCounter)});
+});
+
+io.on('connect', function() { connectCounter++; console.log(connectCounter); });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
